af34b1abdecec7431235855b1663dc759ff4c213
RkShaRkz <darksidemetatron@gmail.com>
2024-03-26 14:56:42 +0100

    QoL improvements; button spam prevented; sending of request seems to work fine
    title says it all, button click listener has been guarded and extracted and button can't be spammed

--- app/src/main/java/com/example/cloudonix/MainActivity.java
+++ app/src/main/java/com/example/cloudonix/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cloudonix.databinding.ActivityMainBinding;
 import com.example.cloudonix.network.SendRequest;
@@ -32,21 +35,54 @@ public class MainActivity extends AppCompatActivity {
         tv.setText(getifaddrs());
 
         Button btnSend = binding.buttonSendRequest;
-        btnSend.setOnClickListener(new View.OnClickListener() {
+        btnSend.setOnClickListener(getButtonClickListener());
+    }
+
+    private View.OnClickListener getButtonClickListener() {
+        return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("SHARK", "button clicked!");
+                v.setClickable(false);
                 String address = getifaddrs();
                 SendRequest request = new SendRequest(new SendRequest.Params(address));
                 AsyncTask<Void, Void, SendRequest.ResponseResult> networkingTask = new AsyncTask<Void, Void, SendRequest.ResponseResult>() {
+                    CountDownTimer timer = new CountDownTimer(3000, 100) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            // we dont care about this
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            // when 3 seconds are up, we need to raise a overlay
+                            //TODO overlay
+                        }
+                    };
+
+                    @Override
+                    protected void onPreExecute() {
+                        super.onPreExecute();
+                        timer.start();
+                    }
+
                     @Override
                     protected SendRequest.ResponseResult doInBackground(Void... voids) {
                         SendRequest.ResponseResult result = request.sendRequest();
                         return result;
                     }
+
+                    @Override
+                    protected void onPostExecute(SendRequest.ResponseResult responseResult) {
+                        super.onPostExecute(responseResult);
+                        v.setClickable(true);
+                        //TODO in case the overlay was opened, close it here
+                        Toast.makeText(getBaseContext(), "Result was success ? "+responseResult.isSuccess(), Toast.LENGTH_SHORT).show();
+                    }
                 }.execute();
 
             }
-        });
+        };
     }
 
     /**
